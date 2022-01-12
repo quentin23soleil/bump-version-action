7bee151ea97e44417dbe21c38ebbc00a412084d2
Quentin Dommerc <dommer.q@gmail.com>
2022-01-12 05:58:51 +0100

    use latest version instead of file

--- dist/index.js
+++ dist/index.js
@@ -63,6 +63,7 @@ const versionFetch = async () => {
   // const result = gitDescribe({
   //   customArguments: ["--tags", "--abbrev=0"]
   // })
+  console.log("result of addRemote:" + await gitClient.addRemote("origin", "git@github.com:quentin23soleil/plex-compose-desktop.git"))
   console.log("result of listRemote:" + JSON.stringify(await gitClient.listRemote()))
   console.log("result of fetch:" + JSON.stringify(await gitClient.fetch()))
   console.log("result of tags:" + JSON.stringify(await gitClient.tags()))
